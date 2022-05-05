38d19a77dcf3f7364456c6e45f31e9403a64acff
ultrarazrab227 <78072227+ultrarazrab227@users.noreply.github.com>
2022-05-05 22:19:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from telegram import *
-from telegram.ext import *
 import telegram
 import vk_api
 import youtube_dl
@@ -25,12 +24,11 @@ last = vk.wall.get(owner_id=public_id, count=1)
 print(admin_id)
 
 
-# print(last)
-# last['items'][0]['id']
+
 def check():
     global last
     req = vk.wall.get(owner_id=public_id, count=1)
-    if req['items'][0]['id'] != 1:
+    if req['items'][0]['id'] != last['items'][0]['id']:
         if req['items'][0]['attachments'][0]['type'] != 'video':
             from_id = req['items'][0]['from_id']
             post_link = "https://vk.com/wall{}_{}".format(from_id, req['items'][0]['id'])
