21a076886d6f6056942e7cad877ea28fc45fa50a
ultrarazrab227 <78072227+ultrarazrab227@users.noreply.github.com>
2022-05-12 22:28:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import youtube_dl
 import configparser
 import os
 from time import sleep
+import uuid
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -33,36 +34,35 @@ def check():
             return
         owner_id = req['items'][0]['attachments'][0]['video']['owner_id']
         video_id = req['items'][0]['attachments'][0]['video']['id']
-        text = req['items'][0]['text']
-        ydl_opts = {}
+        text = uuid.uuid4()
         print('https://vk.com/video{}_{}'.format(owner_id, video_id))
         try:
+            ydl_opts = {'outtmpl': f'video/{text}.mp4', 'format': 'mp4'}
             with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                 ydl.download(['https://vk.com/video{}_{}'.format(owner_id, video_id)])
         except:
             post_link = "https://vk.com/wall{}_{}".format(req['items'][0]['id'], owner_id)
             bot.send_message(admin_id, post_link)
             return
-        filename = ""
-        for root, dirs, files in os.walk("."):
-            for name in files:
-                if ".mp4" in name:
-                    filename = name
-        if filename == "":
-            post_link = "https://vk.com/wall{}_{}".format(req['items'][0]['id'], owner_id)
-            bot.send_message(admin_id, post_link)
-            return
-        video = open(filename, 'rb')
+        filename = f"{text}.mp4"
+        video = open(os.path.join('video', filename), 'rb')
+
         try:
+            text = req['items'][0]['text']
             bot.send_video(chat_id, video, caption=text)
         except telegram.error.NetworkError:
             from_id = req['items'][0]['from_id']
             post_link = "https://vk.com/wall{}_{}".format(from_id, req['items'][0]['id'])
             bot.send_message(admin_id, post_link)
         video.close()
-        os.remove(filename)
+        for f in os.listdir('video'):
+            os.remove(os.path.join('video', f))
 
 
 while True:
-    check()
-    sleep(30)
+    try:
+        check()
+    except Exception as ex:
+        print(ex)
+        print("\n\n\n")
+    sleep(3)
